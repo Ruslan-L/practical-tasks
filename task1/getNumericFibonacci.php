b6607fe943d34a72b002558c923e4fee2a3dfe8a
ruslan.liashenka <ruslan.liashenka@innowise-group.com>
2021-11-16 13:06:50 +0300

    Problem №1, task with Fibonacci numbers

---
+++ task1/getNumericFibonacci.php
@@ -0,0 +1,88 @@
+<?php
+class Fibonacci
+{
+    private array $methods;
+
+    public function __construct()
+    {
+        $this->methods = ['math', 'loop', 'recursive', 'tailRecursive'];
+    }
+
+    public function getNumeric(string $method, int $position): string
+    {
+        $key = array_search($method, $this->methods, true);
+
+        return $method ? 'Число фибоначи равно: ' . $this->{$this->methods[$key]}($position) : "Неизвестный метод ($method)";
+    }
+
+    public function speedTest(): array
+    {
+        $results = [];
+
+        foreach ($this->methods as $method) {
+            $start = microtime(true);
+            $x     = 0;
+
+            while ($x <= 1000) { $this->{$method}(5); $x++; }
+
+            $speed = (microtime(true) - $start);
+
+            if ($results['fast'] < $speed) {
+                $results += [
+                    'fast' => [
+                        'method' => $method,
+                        'speed'  => $speed
+                    ]
+                ];
+            }
+
+            $results += [
+                $method => "Скорость выполения($method): $speed c."
+            ];
+        }
+
+        $results['fast'] = $results['fast']['method'] . ' - самый быстрый метод, скорость выполнения (' . $results['fast']['speed'] . ' c.)';
+
+        return $results;
+    }
+
+    /* Math */
+    private function math(int $position): int
+    {
+        return round((((sqrt(5) + 1) / 2) ** --$position) / sqrt(5));
+    }
+
+    /* Loop */
+    private function loop(int $position): int
+    {
+        $first = 0;
+        $second = 1;
+
+        for ($i = 1; $i < $position; $i++) {
+            $firstSecond = $second;
+            $second += $first;
+            $first = $firstSecond;
+        }
+
+        return $first;
+    }
+
+    /* Recursive */
+    private function recursive(int $position, int $first = 0, int $second = 1): int
+    {
+        if ($position <= 1) { return $first; }
+
+        return 0 + $this->recursive($position - 1, $second, $second + $first);
+    }
+
+    /* Recursive Tail */
+    private function tailRecursive(int $position, int $first = 0, int $second = 1): int
+    {
+        return $position <= 1 ? $first : $this->tailRecursive($position - 1, $second, $second + $first);
+    }
+}
+
+$fibonacci = new Fibonacci();
+
+echo $fibonacci->getNumeric('loop', 10);
+echo var_dump($fibonacci->speedTest());
