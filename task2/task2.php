641151bee51e779bd45bb792f8566b33bf065fbd
ruslan.liashenka <ruslan.liashenka@innowise-group.com>
2021-11-30 13:07:09 +0300

    Refactoring

--- task2/task2.php
+++ task2/task2.php
@@ -1,7 +1,7 @@
 <?php
 function multipleAddition(int $x): int
 {
-    $number = array_sum(array_map('intval', str_split($x)));
-    return count($number) !== 1 ? multipleAddition($number) : $number;
+    $number = array_sum(array_map('intval', str_split(abs($x))));
+    return strlen($number) === 1 ? $number : multipleAddition($number);
 }
 echo multipleAddition(5689);
