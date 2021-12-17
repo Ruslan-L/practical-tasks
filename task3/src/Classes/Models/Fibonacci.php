e8d692d1fc065e6be7780b7daa76508860338037
ruslan.liashenka <ruslan.liashenka@innowise-group.com>
2021-12-17 18:22:40 +0300

    Add PHPDoc

--- task3/src/Classes/Models/Fibonacci.php
+++ task3/src/Classes/Models/Fibonacci.php
@@ -2,17 +2,28 @@
 
 namespace App\Models;
 
+/**
+ * @author Lishencka Ruslan Konstantinovich
+ */
 class Fibonacci
 {
+    /** @var string First fibonacci number */
     private string $first;
+
+    /** @var string Second fibonacci number */
     private string $second;
 
     public function __construct()
     {
-        $this->first = 0;
-        $this->second = 1;
+        $this->first = '0';
+        $this->second = '1';
     }
 
+    /**
+     * Returns a fibonacci number of at least one hundred characters.
+     *
+     * @return string
+    **/
     public function get(): string
     {
         if (strlen($this->second) >= 100) {
@@ -25,6 +36,16 @@ class Fibonacci
         return $this->get();
     }
 
+    /**
+     * Column addition of Fibonacci numbers
+     *
+     * @param string $first
+     * @param string $second
+     * @param string $enhance
+     * @param string $result
+     *
+     * @return string
+     */
     private function sum(string $first, string $second, string $enhance = '', string $result = ''): string
     {
         $nextValueFirst = strlen($first) > 1 ? substr($first, 0, -1) : '';
